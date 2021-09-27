52ed82d01ea43fa4d0df834ebee88d046b1060ed
Игорь <anapa.igor@yandex.ru>
2021-09-27 19:21:21 +0300

    -port_scanner 0.0.1;

--- main.py
+++ main.py
@@ -13,11 +13,26 @@ def port_scanner(ip, port):
         pass
 
 
-count = 0
+def get_range_ip():
+    ip_list = []
+    user_input = input()
+    res = user_input.split('/')
+    user_ip = res[0][:-1]
+    start_ip = int(res[0].split('.')[-1])
+    end_ip = int(res[1])
+    for ip in range(start_ip, end_ip):
+        ip_list.append(f'{user_ip}{ip}')
+    return ip_list
 
-while count < 11:
-    ip = f'192.168.0.{count}'
-    for port in range(1000):
-        stream = threading.Thread(target=port_scanner, args=(ip, port))
-        stream.start()
-    count += 1
+
+def main():
+    ip_list = get_range_ip()
+    port_list = [80, 25, 100, 333]
+    for ip in ip_list:
+        for port in range(1000):
+            stream = threading.Thread(target=port_scanner, args=(ip, port))
+            stream.start()
+
+
+if __name__ == '__main__':
+    main()
